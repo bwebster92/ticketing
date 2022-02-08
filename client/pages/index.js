6a5d5469eb00a32234f2724db96e3eaafadebef9
Benny Webster <bwebster1992@gmail.com>
2022-02-08 12:40:01 +1100

    Added logging for getServerSideProps

--- client/pages/index.js
+++ client/pages/index.js
@@ -41,6 +41,9 @@ export const getServerSideProps = async (context) => {
   const userRes = await client.get('/api/users/currentuser');
   const ticketRes = await client.get('/api/tickets');
 
+  console.log(userRes);
+  console.log(ticketRes);
+
   return { props: { currentUser: userRes.data, tickets: ticketRes.data } };
 };
 
